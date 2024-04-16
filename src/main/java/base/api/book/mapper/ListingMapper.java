6116762af18065f4c7881101025be4c57d69fdc7
Phan <36981556+phanngl@users.noreply.github.com>
2024-04-16 19:30:27 +1200

    Fix ListingMapper

--- src/main/java/base/api/book/mapper/ListingMapper.java
+++ src/main/java/base/api/book/mapper/ListingMapper.java
@@ -6,13 +6,16 @@ import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ListingMapper {
-//  @Mapping(source = "copyId", target = "copy.id")
+  @Mapping(source = "copyId", target = "copy.id")
+  @Mapping(source = "ownerId", target = "owner.id")
   Listing toEntity(ListingDto listingDto);
 
-//  @Mapping(source = "copy.id", target = "copyId")
+  @Mapping(source = "copy.id", target = "copyId")
+  @Mapping(source = "owner.id", target = "ownerId")
   ListingDto toDto(Listing listing);
 
   @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
   @Mapping(source = "copyId", target = "copy.id")
+  @Mapping(source = "ownerId", target = "owner.id")
   Listing partialUpdate(ListingDto listingDto, @MappingTarget Listing listing);
 }
