900f8dd2e182c597660e81b78d1df23db148ffb9
Phan <36981556+phanngl@users.noreply.github.com>
2024-05-14 21:09:15 +1200

    Allow all origins

--- src/main/java/base/api/system/security/SecurityConfig.java
+++ src/main/java/base/api/system/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -42,6 +43,7 @@ public class SecurityConfig {
       JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
 
     http
+      .cors(Customizer.withDefaults()) // FIXME allowing all origins
       .httpBasic(httpBasic -> httpBasic.disable())
       .formLogin(httpFormLogin -> httpFormLogin.disable())
 
