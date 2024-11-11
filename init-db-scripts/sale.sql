1b4ceebb8099cdd87229888c4351235ba2cd5b24
Thao Nguyen <thaonguyentt.2811@gmail.com>
2024-11-11 20:54:55 +0700

    git show

--- init-db-scripts/sale.sql
+++ init-db-scripts/sale.sql
@@ -46,6 +46,23 @@ create table sales_order_detail
 
 insert into sales_order_detail (id,title,sale_order_id,listing_id,copy_id,price)
 values
-    (1,'test',1,1,1,100000.0);
+    (1,'test',1,1,1,100000.0),
+    (2,'');
+
+["https://i.ibb.co/9Z0j3Bg/Screen-Shot-2024-11-11-at-20-28-45.png",
+"https://i.ibb.co/NSXFBM0/Screen-Shot-2024-11-11-at-20-29-12.png",
+"https://i.ibb.co/2nwyddJ/Screen-Shot-2024-11-11-at-20-29-40.png",
+"https://i.ibb.co/rxXnQR4/Screen-Shot-2024-11-11-at-20-29-53.png",
+"https://i.ibb.co/FnGQXHS/Screen-Shot-2024-11-11-at-20-30-06.png",
+"https://i.ibb.co/BN4Mbmw/Screen-Shot-2024-11-11-at-20-30-19.png",
+"https://i.ibb.co/NKmZHcc/Screen-Shot-2024-11-11-at-20-30-32.png",
+"https://i.ibb.co/GJT1134/Screen-Shot-2024-11-11-at-20-30-44.png",
+"https://i.ibb.co/3FF3c1y/Screen-Shot-2024-11-11-at-20-30-55.png",
+"https://i.ibb.co/cgKf6WS/Screen-Shot-2024-11-11-at-20-31-07.png",
+"https://i.ibb.co/Sy6kD7c/Screen-Shot-2024-11-11-at-20-31-40.png",
+"https://i.ibb.co/W6BWS51/Screen-Shot-2024-11-11-at-20-31-51.png",
+"https://i.ibb.co/vsddnr8/Screen-Shot-2024-11-11-at-20-32-01.png",
+"https://i.ibb.co/XJZLdXC/Screen-Shot-2024-11-11-at-20-32-13.png",
+"https://i.ibb.co/5jsDnGJ/Screen-Shot-2024-11-11-at-20-32-25.png"]
 
 alter sequence sales_order_detail_id_seq restart with 2;
